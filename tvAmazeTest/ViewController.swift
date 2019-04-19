d2586ec3ef7ff57908f342e16758474ac1080979
nicholasSharp539 <naas8905@gmail.com>
2019-04-19 10:00:13 -0400

    Initial Commit

---
+++ tvAmazeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tvAmazeTest
+//
+//  Created by MCS on 4/19/19.
+//  Copyright © 2019 MCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
